4373ae835b724cf5bc46ef02f0c5c11efbe93432
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 17:41:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,14 +6,15 @@ def array_with_two_elements
 	[1, 2]
 end
 
-def first_element
-  taylor_swift[0]
+def first_element(array)
+  array[0]
 end
 
-def third_element
-  taylor_swift[2]
+def third_element(array)
+  array[2]
 end
 
-def last_element
-  taylor_swift[-1]
+def last_element(array)
+  array[-1]
 end
+
