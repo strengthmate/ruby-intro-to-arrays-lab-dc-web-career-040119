3e3a2b5184fddcaf10c5082365efa167d7a0b505
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 17:27:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,13 +1,8 @@
-def instantiate_new_array()
-  
-  my_new_array = Array.new
-  
-  puts my_new_array.inspect
-  
-  my_two_array = ["Object", "Objects"]
-
-  puts my_two_array.inspect
-  
+def instaniate_new_array
+	Array.new
 end
-  
-  
+
+def array_with_two_elements
+	[1, 2]
+end
+
